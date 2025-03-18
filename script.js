6e2d303dcf3999c107579b844e245bfe5b53c5ac
vasudev9896 <vasuiii250@gmail.com>
2025-03-18 10:59:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Handle the "Yes" button click
+document.getElementById('yes-button').addEventListener('click', function () {
+    window.location.href = 'cute_page.html';
+});
+
+// Handle the "No" button click
+document.getElementById('no-button').addEventListener('click', function () {
+    alert("Please reconsider, you're amazing! 😊");
+});
+
